465bb841445ac58a807ac3398c22dfc35e438f90
Luis andrés bolanos <luisandres@bolanos>
2021-01-03 17:18:28 +0100

    Hecho un pipe (filtro) para mostar las lista completadas

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { Lista } from "src/app/models/lista.model";
 import { DeseosService } from "src/app/services/deseos.service";
-
+import { ToastController } from "@ionic/angular";
 @Component({
   selector: "app-listas",
   templateUrl: "./listas.component.html",
@@ -10,15 +10,29 @@ import { DeseosService } from "src/app/services/deseos.service";
 })
 export class ListasComponent implements OnInit {
   @Input() terminada = true;
-  constructor(public deseosService: DeseosService, public router: Router) {}
+  constructor(
+    public deseosService: DeseosService,
+    public router: Router,
+    public toastController: ToastController
+  ) {}
 
   ngOnInit() {}
 
   listaSeleccionada(lista: Lista) {
-    if(this.terminada){
+    if (this.terminada) {
       this.router.navigateByUrl(`/tabs/tab2/agregar/${lista.id}`);
-    }else{
+    } else {
       this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
     }
+  }
+
+  async borrarLista(lista: Lista) {
+    this.deseosService.borrarLista(lista);
+    const toast = await this.toastController.create({
+      message: "Lista borrada",
+      duration: 900,
+      color:"success"
+    });
+    toast.present();
   }
 }
